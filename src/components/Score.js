9ee09f01e9d16dc0040eb50d1d64f1b119ccdc77
AnjaliAgrawal <anjaliagarwal383@gmail.com>
2023-02-28 22:41:47 +0530

    d

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,18 +1,3 @@
-/*import React from 'react';
-import './Score.css';
-
-const Score = ({ score, totalQuestions, restartQuiz }) => {
-  return (
-    <div className="score-section">
-      <h2>Quiz Completed!</h2>
-      <p>You scored {score} out of {totalQuestions}.</p>
-      <button onClick={restartQuiz}>Restart Quiz</button>
-    </div>
-  );
-};
-
-export default Score;
-*/
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Score.css';
