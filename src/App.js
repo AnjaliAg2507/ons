f126ba8df1c943c29efade9582a9ad57e5078844
AnjaliAgrawal <anjaliagarwal383@gmail.com>
2023-02-28 22:19:46 +0530

    fixed quiz button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
+
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import StartScreen from './components/StartScreen';
 import Quiz from './components/Quiz';
 import Score from './components/Score';
-/*import hcbgImage from "./images/b1.png";*/
 import './App.css';
-
 function App() {
   return (
-    <Router>
-   
-      <div className="App" >
-        <Switch>
-          <Route exact path="/" component={StartScreen} />
-          <Route exact path="/Quiz" component={Quiz} />
-          <Route exact path="/score" component={Score} />
-          
-        </Switch>
-      </div>
-     
-    </Router>
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={StartScreen} />
+        <Route exact path="/Quiz" component={Quiz} />
+        <Route exact path="/score" component={Score} />
+      </Switch>
+    </div>
   );
 }
 
