099ab1e1313bfda5c12bcd3067835e0b22228a04
AnjaliAgrawal <anjaliagarwal383@gmail.com>
2023-03-03 23:21:11 +0530

    start screen changes

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -6,6 +6,9 @@ import Quiz from './Quiz';
 function StartScreen() {
   return (
     <div className="StartScreen">
+    
+      <img src={require('../images/logo.png')} alt="Logo" id="logo" height={200} width={200} ></img>
+      
       <h1>Government of Karnataka</h1>
       <p>A fun way to learn maths!</p>
       <div className="button-container">
